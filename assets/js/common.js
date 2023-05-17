b7441ab49cf30907f5c8ac1b9e7c9e3b85e14232
k k <akoh1214@gmail.com>
2023-05-17 18:39:11 +0900

    ギャラリーjs実装

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,3 +10,27 @@ $(function (){
   }
   });
 });
+
+// slick
+$('.section-gallery__area').slick({
+  autoplay:true,
+  autoplaySpeed: 0,
+  arrows:false,
+  cssEase: 'linear',
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  speed: 8000,
+});
+
+$(function() {
+  $('.').slick({
+      fade: true,
+      autoplay: true,
+      speed: 1500,
+      autoplaySpeed : 2000,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      arrows: false,
+  })
+});
