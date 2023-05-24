1884cec9e62e4029ee220b0f363cbe1e9f3ce8d4
k k <akoh1214@gmail.com>
2023-05-24 14:50:12 +0900

    ajax-loader追加

--- assets/js/common.js
+++ assets/js/common.js
@@ -121,10 +121,33 @@ checkBreakPointGallery();
 // ハンバーガメニュー
 const ham = document.querySelector('#js-hamburger');
 const nav = document.querySelector('#js-nav');
+const li = document.querySelector('.nav-items__item');
 
-ham.addEventListener('click', function () {
+ham.addEventListener('click', function addActive() {
 
   ham.classList.toggle('active');
   nav.classList.toggle('active');
 
+});
+
+// メニューからリンクに飛んだらトグルをしまう
+// スマートフォンの場合
+$(document).on('click', '.nav-items__item', function(){
+  alert('Button is clicked');
+  ham.classList.toggle('active');
+  nav.classList.toggle('active');
+
+});
+
+$(function showSetting(){
+  $(window).resize(function() {
+    var $hn =$('.header__nav');
+    var width = $(window).width();
+    if (width <= 1400){
+      // console.log("スマホ用表示");
+    } else{
+      // console.log("PC表示");
+    $hn.removeAttr('style');
+    }
+  });
 });
